80334d3fd56f0272d602da834e070d890051e74e
Phil Smith <philip@philipsmith.ie>
2022-05-22 20:04:18 +0100

    Fixes issue #4 emitter null errors

--- src/mappings/gala1155.ts
+++ src/mappings/gala1155.ts
@@ -54,7 +54,7 @@ function registerTransfer(
 {
 	let token      = fetchERC1155Token(contract, id)
 	let ev         = new ERC1155Transfer(events.id(event).concat(suffix))
-	ev.emitter     = token.id
+	ev.emitter     = contract.id
 	ev.transaction = transactions.log(event).id
 	ev.timestamp   = event.block.timestamp
 	ev.contract    = contract.id
